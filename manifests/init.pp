e2f69d35b363cc3d11b6e4af3f477bdc98fd7bf5
Helen Griffiths <16269977+threepistons@users.noreply.github.com>
2021-06-16 17:58:43 +0100

    Add uninstaller and prevent the module from attempting to install on Pis

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,45 @@
-# @summary A short summary of the purpose of this class
+# @summary Puppet manifest to install Microsoft Defender for Endpoint on Linux.
 #
-# A description of what this class does
+# @param ensure False uninstalls the agent, true installs it. Default: true
+# @param channel The release channel based on your environment, insiders-fast, insiders-slow, or prod. Default: prod
 #
 # @example
-#   include ms_defender_atp_agent
-class ms_defender_atp_agent {
+#   include ms_defender_atp_agent # installs
+#   class { 'ms_defender_atp_agent': ensure => false } # removes
+class ms_defender_atp_agent (
+  $ensure  = true,
+  $channel = prod,
+) {
+
+  case $ensure {
+
+    false:   { # Get the uninstaller out of the way early in the code
+
+      package { 'mdatp':
+        ensure => 'purged',
+      }
+
+    }
+
+    default: {
+
+      # I run a lot of armhf Pis and this endpoint thing won't work on them because the [packages](https://packages.microsoft.com/debian/10/prod/pool/main/m/mdatp/)
+      # only come as amd64, so filter out non-amd64 architectures.
+
+      case $::facts['os']['architecture']  {
+
+        'amd64': {
+          # Do useful things
+        }
+
+        default: {
+          notify { 'needs_amd64':
+            message => "${::facts['os']['architecture']} is currently not supported."
+          }
+        }
+      }
+    }
+
+  }
+
 }
--- spec/classes/ms_defender_atp_agent_spec.rb
+++ spec/classes/ms_defender_atp_agent_spec.rb
@@ -1,13 +1,17 @@
-# frozen_string_literal: true
-
 require 'spec_helper'
 
 describe 'ms_defender_atp_agent' do
   on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
+    context "uninstallation on  #{os}" do
       let(:facts) { os_facts }
+      let :params do
+        {
+          'ensure' => false, # true/false needs to be a bare keyword in Rspec
+        }
+      end
 
       it { is_expected.to compile.with_all_deps }
+      it { is_expected.to contain_package('mdatp').with('ensure' => 'purged') }
     end
   end
 end
